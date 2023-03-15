68f29aa053fe8d726427b7864b4171682a16baa4
Siva9727 <sivasakthi203023@gmail.com>
2023-03-15 11:41:31 +0530

    streams and comparator used

--- src/Library/Book.java
+++ src/Library/Book.java
@@ -4,29 +4,31 @@ public class Book {
     private final String title;
     private final String author;
     private final int yearOfPublish;
-    public  Book(String title,String author, int yearOfPublish){
+
+    public Book(String title, String author, int yearOfPublish) {
         this.title = title;
         this.author = author;
         this.yearOfPublish = yearOfPublish;
 
     }
-//    add methods to get the book details
-    public String getAuthor(){
+
+    //    add methods to get the book details
+    public String getAuthor() {
         return author;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public int getYearOfPublish(){
+    public int getYearOfPublish() {
         return yearOfPublish;
     }
 
-    public void display(){
-        System.out.print("title: "+this.title);
+    public void display() {
+        System.out.print("title: " + this.title);
 //        System.out.println();
-        System.out.print("--> author: " +this.author);
+        System.out.print("--> author: " + this.author);
 //        System.out.println();
         System.out.print("-->Year of Publish: " + this.yearOfPublish);
         System.out.println();
