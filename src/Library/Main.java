a038aa38b413575eef2d524b1575079c8a1cf9b2
Siva9727 <sivasakthi203023@gmail.com>
2023-03-17 15:35:12 +0530

    used jdbc

--- src/Library/Main.java
+++ src/Library/Main.java
@@ -1,113 +1,80 @@
 package Library;
+/*
+******************************************************************************
+* ********* Check the BookShop.java file for the methods used ****************
+ */
 
-
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    static BookShop2 books = new BookShop2();
-    static Scanner in = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BookShop bookShop = new BookShop();
+        Scanner in = new Scanner(System.in);
+        /*
+        1. "To Kill a Mockingbird", "Harper Lee", 1960
+        2. "1984", "George Orwell", 1949
+        3. "The Great Gatsby", "F. Scott Fitzgerald", 1925
+        4. "Pride and Prejudice", "Jane Austen", 1813
+        5. "One Hundred Years of Solitude", "Gabriel Garcia Marquez", 1967
+        6. "The Catcher in the Rye", "J.D. Salinger", 1951
+        7. "The Lord of the Rings", "J.R.R. Tolkien", 1954
+        8. "Beloved", "Toni Morrison", 1987
+        9. "The Color Purple", "Alice Walker", 1982
+        10. "The Handmaid's Tale", "Margaret Atwood", 1985
+        11. "Harry Potter and the Philosopher's Stone", "J.K. Rowling", 1997
+        12. "Harry Potter and the Chamber of Secrets", "J.K. Rowling", 1998
+        13. "Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", 1999
+        14. "Harry Potter and the Goblet of Fire", "J.K. Rowling", 2000
+        15. "Angela's Ashes", "Frank McCourt", 1996
+        16.
+
+         */
+        // add books
+//        bookShop.addBooks("To Kill a Mockingbird", "Harper Lee", 1960);
+//        bookShop.addBooks("1984", "George Orwell", 1949);
+//        bookShop.addBooks("The Great Gatsby", "F. Scott Fitzgerald", 1925);
+//        bookShop.addBooks("Pride and Prejudice", "Jane Austen", 1813);
+//        bookShop.addBooks("One Hundred Years of Solitude", "Gabriel Garcia Marquez", 1967);
+//        bookShop.addBooks("The Catcher in the Rye", "J.D. Salinger", 1951);
+//        bookShop.addBooks("The Lord of the Rings", "J.R.R. Tolkien", 1954);
+//        bookShop.addBooks("Beloved", "Toni Morrison", 1987);
+//        bookShop.addBooks("The Color Purple", "Alice Walker", 1982);
+//        bookShop.addBooks( "The Handmaid's Tale", "Margaret Atwood", 1985);
+//        bookShop.addBooks("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 1997);
+//        bookShop.addBooks("Harry Potter and the Chamber of Secrets", "J.K. Rowling", 1998);
+//        bookShop.addBooks("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", 1999);
+//        bookShop.addBooks("Harry Potter and the Goblet of Fire", "J.K. Rowling", 2000);
+//        bookShop.addBooks("Angela's Ashes", "Frank McCourt", 1996);
+
+//
+
+//        bookShop.removeBooks();
+//        bookShop.sortedList();
+//        bookShop.filterBook('T');
+//        bookShop.addBooks("Angela", "Frank", 1996);
+//        bookShop.updateBook();
 
 
-        // add books here
+        System.out.println("Select : \n1 for list all books in database \n2 for sorted list \n3 for search \n4 for filter \n5 for update the details of existing book");
 
-        books.addBooks("To Kill a Mockingbird", "Harper Lee", 1960);
-        books.addBooks("1984", "George Orwell", 1949);
-        books.addBooks("The Great Gatsby", "F. Scott Fitzgerald", 1925);
-        books.addBooks("Pride and Prejudice", "Jane Austen", 1813);
-        books.addBooks("One Hundred Years of Solitude", "Gabriel Garcia Marquez", 1967);
-        books.addBooks("The Catcher in the Rye", "J.D. Salinger", 1951);
-        books.addBooks("The Lord of the Rings", "J.R.R. Tolkien", 1954);
-        books.addBooks("Beloved", "Toni Morrison", 1987);
-        books.addBooks("The Color Purple", "Alice Walker", 1982);
-        books.addBooks("The Handmaid's Tale", "Margaret Atwood", 1985);
-        books.addBooks("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 1997);
-        books.addBooks("Harry Potter and the Chamber of Secrets", "J.K. Rowling", 1998);
-        books.addBooks("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", 1999);
-        books.addBooks("Harry Potter and the Goblet of Fire", "J.K. Rowling", 2000);
-        books.addBooks("Angela's Ashes", "Frank McCourt", 1996);
-        books.addBooks("Angela's Ashes", "Anna", 1996);
-
-
-
-
-        System.out.println("select \n1 for search \n2 for view all books \n3 for sorted book titles \n4 for filter book titles");
-
-        int n = in.nextInt();
-        System.out.println(n);
-        switch (n) {
+        int option = in.nextInt();
+        switch (option) {
             case 1 -> {
-                System.out.println("enter name of the book?");
-                in.nextLine();
-                String title = in.nextLine().trim();
-                System.out.println("enter author's name ?");
-                String author = in.nextLine().trim();
-                search(author, title);
+                List<Book> list = bookShop.getAllBooks();
+                list.forEach(Book::display);
             }
-            case 2 -> viewAllBooks();
-            case 3 -> sortedList();
+            case 2 -> bookShop.sortedList();
+            case 3 -> bookShop.searchBook();
             case 4 -> {
-                System.out.println("Enter the character between A to Z");
+                System.out.println("enter the character for the filter A-Z");
                 char c = in.next().trim().charAt(0);
-                filter(c);
+                bookShop.filterBook(c);
             }
-            default -> System.out.println("Invalid option");
-        }
-
-
-    }
-
-
-
-    // to filter
-
-    private static void filter(char c) {
-        List<Book> list = books.getAllBooks();
-        List<Book> names;
-        names = list.stream()
-                .filter(book -> book.getTitle().charAt(0)==Character.toLowerCase(c) || book.getTitle().charAt(0) == Character.toUpperCase(c))
-                .collect(Collectors.toList());
-        for (Book a: names){
-            System.out.println(a.getTitle()+" : "+ a.getAuthor());
-        }
-
-    }
-
-    // to sort
-
-    private static void sortedList() {
-        List<Book> list = books.getAllBooks();
-        List<Book> names;
-        names = list.stream()
-                .sorted(Comparator.comparing(Book::getTitle).thenComparing(Book::getAuthor))
-                .collect(Collectors.toList());
-        for (Book a : names){
-            System.out.println(a.getTitle()+" : "+ a.getAuthor() + " : "+a.getYearOfPublish());
+            case 5 -> bookShop.updateBook();
+            default -> System.out.println("Invalid Options");
         }
     }
-
-    // to get all books
-
-    public static void viewAllBooks() {
-        List<Book> list = books.getAllBooks();
-        list.forEach(Book::display);
-    }
-
-    // to search
-
-    public static void search(String author, String title) {
-        List<Book> list = books.getAllBooks();
-        List<Book> names;
-        names = list.stream()
-                .filter(book -> book.getAuthor().equalsIgnoreCase(author) && book.getTitle().equalsIgnoreCase(title))
-                .collect(Collectors.toList());
-
-        for (Book a : names){
-            a.display();
-        }
-    }
-
 
 }
